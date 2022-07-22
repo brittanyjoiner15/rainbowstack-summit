d3e64a3834726729017161b1ddde4fd002ea2e9d
Brittany Joiner <brittanyjoiner15@gmail.com>
2022-07-22 15:46:18 -0500

    framework with styled header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { EuiProvider } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_dark.css";
-import "@elastic/eui/dist/eui_theme_light.css";
 import React from "react";
 import MainPage from "./components/MainPage";
 
-function App({}) {
+function App() {
   return (
-    <EuiProvider colorMode="light">
+    <EuiProvider colorMode="dark">
       <MainPage />
     </EuiProvider>
   );
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,44 +1,130 @@
 import {
   EuiButton,
+  EuiFlexGroup,
   EuiPage,
   EuiPageBody,
   EuiPageContent,
   EuiPageContentBody,
   EuiPageHeader,
-  EuiPageSideBar,
+  EuiText,
 } from "@elastic/eui";
-import React from "react";
-import DetailsPanel from "./DetailsPanel";
-import SideNav from "./SideNav";
+import { React, useState } from "react";
+import "../imgs/pride.png";
 
 const cta = <EuiButton fill>Add to Calendar</EuiButton>;
 
-function MainPage({}) {
+function MainPage() {
+  const [selectedTab, setSelectedTab] = useState("event");
+
+  const onSelectedTabChanged = (id) => {
+    setSelectedTab(id);
+  };
+  const showSelectedContent = (tab) => {
+    switch (selectedTab) {
+      case "event":
+        return tabs[0].content;
+      case "speakers":
+        return tabs[1].content;
+      case "talks":
+        return tabs[2].content;
+      default:
+        return tabs[0].content;
+    }
+  };
+
+  const tabs = [
+    {
+      id: "event",
+      isSelected: selectedTab === "event",
+      label: "Event",
+      onClick: () => onSelectedTabChanged("event"),
+      content: (
+        <>
+          <EuiText>{selectedTab}</EuiText>
+        </>
+      ),
+    },
+    {
+      id: "speakers",
+      isSelected: selectedTab === "speakers",
+      label: "Speakers",
+      onClick: () => onSelectedTabChanged("speakers"),
+      content: (
+        <>
+          <EuiText>{selectedTab}</EuiText>
+        </>
+      ),
+    },
+    {
+      id: "talks",
+      isSelected: selectedTab === "talks",
+      label: "Talks",
+      onClick: () => onSelectedTabChanged("talks"),
+      content: (
+        <>
+          <EuiText>{selectedTab}</EuiText>
+        </>
+      ),
+    },
+  ];
+
+  const makeRainbowText = () => {
+    const colors = [
+      "#FF1616",
+      "#F7B012",
+      "#FFDE59",
+      "#7ED957",
+      "#38B6FF",
+      "#CB6CE6",
+      "#8C52FF",
+      "#FF1616",
+      "#F7B012",
+      "#FFDE59",
+      "#7ED957",
+      "#38B6FF",
+    ];
+    const letters = "RAINBOWSTACK".split("");
+    const rainbowedLetters = letters.map((letter, index) => {
+      return (
+        <span style={{ color: `${colors[index]}`, fontFamily: "Inter" }}>
+          {letter}
+        </span>
+      );
+    });
+    return (
+      <span>
+        <strong>{rainbowedLetters} SUMMIT</strong>
+      </span>
+    );
+  };
+
   return (
     <EuiPage paddingSize="none">
-      <EuiPageSideBar paddingSize="l" sticky>
-        <SideNav />
-      </EuiPageSideBar>
-      <EuiPageBody panelled>
-        <EuiPageHeader
-          restrictWidth
-          iconType="logoElastic"
-          pageTitle="Rainbowstack Summit"
-          rightSideItems={[cta]}
-          tabs={[{ label: "Tab 1", isSelected: true }, { label: "Tab 2" }]}
-        />
-        <EuiPageContent
-          hasBorder={false}
-          hasShadow={false}
-          paddingSize="none"
-          color="transparent"
-          borderRadius="none"
-        >
-          <EuiPageContentBody restrictWidth>
-            <DetailsPanel />
-          </EuiPageContentBody>
-        </EuiPageContent>
-      </EuiPageBody>
+      <EuiFlexGroup className="eui-fullHeight">
+        <EuiPageBody panelled>
+          <EuiPageHeader
+            restrictWidth
+            iconType="https://user-images.githubusercontent.com/16166290/180519614-5dcfa272-e38d-4228-964e-a1ddf9c85ede.png"
+            pageTitle={makeRainbowText()}
+            rightSideItems={[cta]}
+            tabs={tabs}
+          />
+
+          <EuiPageContent
+            hasBorder={false}
+            hasShadow={false}
+            paddingSize="none"
+            color="transparent"
+            borderRadius="none"
+            verticalPosition="center"
+            horizontalPosition="center"
+          >
+            <EuiPageContentBody>
+              {showSelectedContent(selectedTab)}
+            </EuiPageContentBody>
+          </EuiPageContent>
+        </EuiPageBody>
+      </EuiFlexGroup>
     </EuiPage>
   );
 }
