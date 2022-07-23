d5fb748817ffd8f730d480d447244e6f7cb41dc4
Brittany Joiner <brittanyjoiner15@gmail.com>
2022-07-22 20:02:29 -0500

    speaker panel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { EuiProvider } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_dark.css";
 import React from "react";
+import "./App.css";
 import MainPage from "./components/MainPage";
 
 function App() {
